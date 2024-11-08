d223fff2f3208718bde04b06cd02f12895c76574
oignontuba <thomas.cand@outlook.fr>
2024-11-09 00:32:03 +0100

    Delete scripttp.js

--- scripttp.js
+++
@@ -1,6 +0,0 @@
-const customCursor = document.querySelector('.custom-cursor');
-
-// Mettre à jour la position du curseur personnalisé en fonction de la souris
-document.addEventListener('mousemove', (e) => {
-    customCursor.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`;
-});
